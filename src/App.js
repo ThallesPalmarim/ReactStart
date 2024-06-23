c2bf3c5106926e5812efe1cbbe4771a413d0fe85
Thalles Palmarim <thallesrpaugusto@gmail.com>
2024-06-23 16:47:46 -0300

    Inicializando o react

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Escrevi e saí correndo
+          Escrevi e saí correndo haha
         </p>
         <a
           className="App-link"
