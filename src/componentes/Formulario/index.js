71a89195d86d0a24da74dea99f4a46961e940c07
Thalles Palmarim <thallesrpaugusto@gmail.com>
2024-06-24 22:32:23 -0300

    Submit do botao funcionando e required dos items do formulario

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -12,14 +12,17 @@ const Formulario = () => {
         'Mobile',
         'Inovação e Gestão'
     ]
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+    } 
     return (
         <section className="formulario">
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <CampoTexto label="Nome" placeholder="Digite seu nome"></CampoTexto>
-                <CampoTexto label="Cargo" placeholder="Digite seu cargo"></CampoTexto>
+                <CampoTexto obrigatorio ={true} label="Nome" placeholder="Digite seu nome"></CampoTexto>
+                <CampoTexto obrigatorio ={true} label="Cargo" placeholder="Digite seu cargo"></CampoTexto>
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"></CampoTexto>
-                <ListaSuspensa itens={times} label='Time'></ListaSuspensa>
+                <ListaSuspensa obrigatorio ={true} itens={times} label='Time'></ListaSuspensa>
                 <Botao>Criar Card</Botao>
             </form>
         </section>
