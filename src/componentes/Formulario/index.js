8f79aa2690f5231c2ebfc05126ffeabb197f5942
Thalles Palmarim <thallesrpaugusto@gmail.com>
2024-06-26 13:38:22 -0300

    escondendo times vazios

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -16,6 +16,10 @@ const Formulario = (props) => {
         props.aoColaboradorCadastrado({
             nome, cargo, imagem, time
         })
+        setNome('')
+        setCargo('')
+        setImagem('')
+        setTime('')
     } 
     return (
         <section className="formulario">
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -2,17 +2,26 @@ import Colaborador from '../Colaborador'
 import './Time.css'
 
 const Time = (props) => {
+    const css = { backgroundColor: props.corSecundaria };
+
     return (
         <>
-        <section className='time' style={{backgroundColor: props.corSecundaria}}>
-            <h3 style={{borderColor: props.corPrimaria}}>{props.nome}</h3>
-            <div className='colaboradores'>
-            {props.colaboradores.map(colaborador => <Colaborador nome={colaborador.nome} cargo={colaborador.cargo} imagem ={colaborador.imagem} ></Colaborador>)}
-            </div>
-        </section>
-        
+            {props.colaboradores.length > 0 ? (
+                <section className='time' style={css}>
+                    <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+                    <div className='colaboradores'>
+                        {props.colaboradores.map(colaborador => (
+                            <Colaborador
+                                nome={colaborador.nome}
+                                cargo={colaborador.cargo}
+                                imagem={colaborador.imagem}
+                            />
+                        ))}
+                    </div>
+                </section>
+            ) : null}
         </>
-    )
+    );
 }
 
 export default Time
